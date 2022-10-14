26e8afdf92cea4854b953abb6bbf9ded09b49eb6
Isaac Saunders <isaac.saunders@mmtdigital.co.uk>
2022-10-14 10:58:08 +0100

    Added Google Cloud TF Provider: Fri 14 Oct 2022 10:58:08 BST

---
+++ providers.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  project = "responsive-hall-240812"
+  region  = "europe-west2"
+}
+
+# module "kubernetes-engine" {
+#   source  = "terraform-google-modules/kubernetes-engine/google"
+#   version = "23.2.0"
+#   # insert the 6 required variables here
+# }
+
