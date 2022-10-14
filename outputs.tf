389d82de0bd08413101af93907f497f739623246
Isaac Saunders <isaac.saunders@mmtdigital.co.uk>
2022-10-14 15:20:40 +0100

    Updating config: Fri 14 Oct 2022 15:20:40 BST

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,20 @@ output "project_id" {
   description = "GCloud Project ID"
 }
 
-output "kubernetes_cluster_name" {
-  value       = google_container_cluster.primary.name
-  description = "GKE Cluster Name"
-}
+# output "kubernetes_cluster_name" {
+#   value       = google_container_cluster.primary.name
+#   description = "GKE Cluster Name"
+# }
 
-output "kubernetes_cluster_host" {
-  value       = google_container_cluster.primary.endpoint
-  description = "GKE Cluster Host"
-}
+# output "kubernetes_cluster_host" {
+#   value       = google_container_cluster.primary.endpoint
+#   description = "GKE Cluster Host"
+# }
+
+# output "gcr_location" {
+#   value = data.google_container_registry_repository.web-app.repository_url
+# }
+
+# output "gcr_location" {
+#   value = data.google_container_registry_image.ruby.image_url
+# }
--- variables.tf
+++ variables.tf
@@ -10,8 +10,24 @@ variable "project_id" {
 variable "env" {
   description = "Environment assigned to resrouces."
   type        = string
-    validation {
+  validation {
     condition     = length(var.env) > 0 && contains(["dev", "prod"], var.env)
     error_message = "Environment must be either dev or prod."
   }
+}
+
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  sensitive   = true
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
 }
