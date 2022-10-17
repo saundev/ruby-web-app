7e9bb9f0302a9d3897d3abb813726da6c88fcd34
Isaac Saunders <isaac.saunders@mmtdigital.co.uk>
2022-10-17 21:52:13 +0100

    Tidy up code: Mon 17 Oct 2022 21:52:13 BST

--- modules/gke/gke-cluster.tf
+++ modules/gke/gke-cluster.tf
@@ -8,7 +8,7 @@ resource "google_container_cluster" "primary" {
   subnetwork               = google_compute_subnetwork.subnet.name
 }
 
-# Separately Managed Node Pool
+# Node Pool within cluster
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
   location   = var.region
@@ -33,24 +33,3 @@ resource "google_container_node_pool" "primary_nodes" {
     }
   }
 }
-
-
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
-
-# provider "kubernetes" {
-#   load_config_file = "false"
-
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
-
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
-
--- modules/k8s/kubernetes.tf
+++ modules/k8s/kubernetes.tf
@@ -5,9 +5,7 @@ data "terraform_remote_state" "gke" {
 
   }
 }
-# Configure kubernetes provider with Oauth2 access token.
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
-# This fetches a new token, which will expire in 1 hour.
+
 data "google_client_config" "default" {}
 
 data "google_container_cluster" "ruby_cluster" {
